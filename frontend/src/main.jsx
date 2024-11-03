73e1880abf17434b957baf1b1eee95ee429f0216
Xing-Kai <xingkai414@gmail.com>
2024-11-03 15:12:43 +0800

    4rd commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,15 +10,16 @@
 //)
 
 
-import { Provider } from "@/components/ui/provider"
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+//import './index.css'
+import { ChakraProvider } from "@chakra-ui/react";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider>
+    <ChakraProvider>
       <App />
-    </Provider>
-  </React.StrictMode>,
-)
+    </ChakraProvider>
+  </React.StrictMode>
+);
