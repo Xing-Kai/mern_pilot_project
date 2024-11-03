bd07b19b2d790da0d809d4d7f5f192f625831015
Xing-Kai <xingkai414@gmail.com>
2024-11-03 22:45:21 +0800

    5th commit

---
+++ frontend/src/pages/CreatePage.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react";
+import { useState } from 'react'; 
+
+
+const CreatePage = () => {
+  return (
+    <div>CreatePage</div>
+  )
+}
+
+export default CreatePage
